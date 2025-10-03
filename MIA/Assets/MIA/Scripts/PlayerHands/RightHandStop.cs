712c70bc560754105eb3c050f1854c5e6fbfd14f
Arseniy <154590265+Daliamha@users.noreply.github.com>
2025-10-03 17:55:51 +0700

    small update

--- MIA/Assets/MIA/Scripts/PlayerHands/RightHandStop.cs
+++ MIA/Assets/MIA/Scripts/PlayerHands/RightHandStop.cs
@@ -7,15 +7,20 @@ public class RightHandStop : MonoBehaviour
     [Space]
     public float upOffset;
     public float forwardOffset;
+    public float rotationOffset;
 
 
     private void Update()
     {
         if (rightHandTransform.position.y - playerTransform.position.y >= upOffset &&
             Mathf.Abs(rightHandTransform.position.x - playerTransform.position.x) < forwardOffset &&
-            Mathf.Abs(rightHandTransform.position.z - playerTransform.position.z) < forwardOffset)
+            Mathf.Abs(rightHandTransform.position.z - playerTransform.position.z) < forwardOffset /*&&
+            Mathf.Abs(rightHandTransform.rotation.eulerAngles.y) > 50 && 
+            Mathf.Abs(rightHandTransform.rotation.eulerAngles.y) < 90 &&
+            Mathf.Abs(rightHandTransform.rotation.eulerAngles.x) > 110 &&
+            Mathf.Abs(rightHandTransform.rotation.eulerAngles.x) < 150*/)
         {
-            Debug.Log("BLYHA MUHA NAM PIZDA");
+            Debug.Log("cars stoiat");
         }
     }
 }
